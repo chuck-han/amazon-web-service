721013431e4535aa284f220633d0ba409077ade7
mightyjava <jeetparmar949@gmail.com>
2017-03-11 14:37:42 +0530

    Update Constants.java

--- src/com/mightyjava/aws/constants/Constants.java
+++ src/com/mightyjava/aws/constants/Constants.java
@@ -2,6 +2,6 @@ package com.mightyjava.aws.constants;
 
 public class Constants {
 	//this is just an sample
-	public static final String ACCESS_KEY = "AKIAIZR6UA3CFKDDRHDQ";
-	public static final String SECRET_KEY = "4OROY/TijdgvEDt0we0iPPdYUiG0mpjZVo0ZYSLE";
+	public static final String ACCESS_KEY = "XXXXXXXXXXXXX";
+	public static final String SECRET_KEY = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
 }
